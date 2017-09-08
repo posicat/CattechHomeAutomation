30d639c73b5b62de912a758122a4511d0d386d68
posicat <posicat@pobox.com>
2017-09-07 20:46:04 -0500

    Adding in the Java part of this code.

---
+++ HomeAutomationHub/src/org/cattech/homeAutomation/communicationHub/Hub.java
@@ -0,0 +1,23 @@
+package org.cattech.homeAutomation.communicationHub;
+
+public class Hub {
+
+	private static ChannelController controller = new ChannelController();
+	
+	public static void main(String[] args) {
+		NodeSocketConnectionManager server = new NodeSocketConnectionManager(10042,controller);
+		new Thread(server).start();
+
+		System.out.println("Listening...");
+		try {
+			while (!server.isStopped()) {
+				Thread.sleep(100);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Stopping Server");
+		server.stop();
+	}
+
+}
