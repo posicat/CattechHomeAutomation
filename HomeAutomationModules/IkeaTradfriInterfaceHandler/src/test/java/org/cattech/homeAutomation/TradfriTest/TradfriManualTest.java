b396f1d2be1da201c5a8f35560138ce69aa4512a
Mark Balliet <posicat@pobox.com>
2020-07-20 04:52:36 -0500

    Working reception of Tradfri light events.

--- HomeAutomationModules/IkeaTradfriInterfaceHandler/src/test/java/org/cattech/homeAutomation/TradfriTest/TradfriManualTest.java
+++ HomeAutomationModules/IkeaTradfriInterfaceHandler/src/test/java/org/cattech/homeAutomation/TradfriTest/TradfriManualTest.java
@@ -1,11 +1,12 @@
 package org.cattech.homeAutomation.TradfriTest;
 
-import org.cattech.HomeAutomation.IkeaTradfriInterfaceHandler.TradfriInterfaceHandler;
+import org.cattech.HomeAutomation.TradfriInterfaceHandler.TradfriInterfaceHandler;
 import org.cattech.homeAutomation.configuration.HomeAutomationConfigurationException;
 import org.cattech.homeAutomation.moduleBaseTest.BaseTestForModules;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 
 public class TradfriManualTest extends BaseTestForModules {
 	TradfriInterfaceHandler tradfreeHandler;
@@ -21,15 +22,17 @@ public class TradfriManualTest extends BaseTestForModules {
 		registerChannel(testInterface, new String[] { "testEventHandler" });
 	}
 
+	@Override
 	@After
 	public void tearDown() throws Exception {
 		tradfreeHandler.setRunning(false);
 	}
 
 	@Test
-	public void testCanTalkToLocalTradfriHub() throws HomeAutomationConfigurationException {
-		controller.getConfig().getProps().setProperty("homeAutomation.config", "/etc/homeAutomation");
-		controller.getConfig().loadConfiguration();
+	public void testCanTalkToLocalTradfriHub() throws HomeAutomationConfigurationException, InterruptedException {
+		while(tradfreeHandler.isRunning()) {
+			Thread.sleep(1000);
+		}
 
 	}
 
